d328253ab5a85ee8e2969dca68c36510e66202f5
João Agostinho <63238478+KrOOb-5@users.noreply.github.com>
2023-06-18 19:13:58 +0100

    More digits and cleaning some elements

--- scripts/main.js
+++ scripts/main.js
@@ -11,10 +11,12 @@ const number7 = document.querySelector("#n7");
 const number8 = document.querySelector("#n8");
 const number9 = document.querySelector("#n9");
 const opTimes = document.querySelector("#times");
-const opTotal = document.querySelector("#total");
+const opTotal = document.querySelector("#totalbut");
 const number0 = document.querySelector("#n0");
 const opPow = document.querySelector("#pow");
 const opDivide = document.querySelector("#divide");
+
+//variables
 
 let text = null; 
 
@@ -26,23 +28,34 @@ let result = null;
 
 let operationFunction = null;
 
+//array
 const numbers = [];
 numbers.push(number0, number1, number2, number3, number4, number5, number6, number7, number8, number9);
 
-
-for (let i = 0; i < numbers.length; i++){
+//text fetch number consoante a posicao no index [i]
+for (let i = 0; i < numbers.length; i++){ 
     function incrementation (){
-        if (text === null) {
-            text = i;
+        if (operation === null) { //enquanto a operação for nula estas no primeiro operando
+            if (text === null){
+                text = ""; //passa a var para uma string pra depois juntar os "dois numeros"
+            }
+            text = text.concat(i); //junta as variaveis todas com concat (funciona so em strings)          
         } else {
-            text2 = i;
+            if (text2 === null){
+                text2 = "";
+            }
+            text2 = text2.concat(i);
         }
         write();
     }
-    numbers[i].addEventListener("click", incrementation); // 
+    numbers[i].addEventListener("click", incrementation);  
 }
 
 
+
+
+
+//Math functions
 function plus (){
     operation = "+";
     operationFunction = function (a,b){return a + b;};
@@ -74,24 +87,8 @@ function pow (){
 }
 
 function total (){
-    // if(operation === "+"){
-    //     result = text + text2;
-
-    // }else if (operation === "-") {
-    //     result = text - text2;
-    
-    // }else if (operation === "*") {
-    //     result = text * text2;
-    
-    // }else if (operation === "/"){
-    //     result = text / text2;
-
-    // }else if (operation === "^") {
-    //     result = Math.pow(text, text2);
-
-    // }
     if (operationFunction && text2 !== null){ // previne que chame a funcao antes que a propria funcao esteja definida
-        result = operationFunction(text, text2);
+        result = operationFunction(+text, +text2); //resultado, +var para tornar a var num numero no resultado
         write();
     }
 
@@ -111,6 +108,16 @@ opPow.addEventListener("click", pow);
 opTotal.addEventListener("click", total);
 
 function write (){
+    /* if(operation === null){
+        if(text2 === null || text === null){
+            myText.textContent = `${text}`;
+        }else{
+            myText.textContent = `${text} ${operation} ${text2}`;
+    }
+    }else{
+        
+    } */
+    
     myText.textContent = `${text} ${operation} ${text2} = ${result}`;
-}
 
+}
